3b48bbdc857cddc509409adfc844173ce666870c
Hugo Duque <hugo.duque@correo.unimet.edu.ve>
2024-07-25 22:30:11 -0400

    feat: Twitter tabs

---
+++ projects/00-hola-mundo/src/App.jsx
@@ -0,0 +1,8 @@
+import { TwitterFollowCard } from "./TwitterFollowCard"
+export function App(){
+    return (
+    <div className="App">
+    <TwitterFollowCard  userName={'midudev'} name={'Hugo Duque'}/>
+    <TwitterFollowCard  userName={'midudev'} name={'Hugo Duque'}/>
+    </div>)
+}
